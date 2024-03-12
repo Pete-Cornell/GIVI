bef9c506bf1c9e29aab5777b7ffcf598b4c3f42b
Pete-Cornell <Givi2kutibashvili@gmail.com>
2024-03-12 19:49:02 +0400

    add to Readme.md

--- Script.js
+++ Script.js
@@ -1,9 +1,57 @@
-console.log("Hello World!!");
-var a=1
-a=1
+/*const numbers =[1,2,3,4,5,6,7];
+console.log(numbers);
+
+let a=84
 console.log(a);
 {
-   a=2
-    console.log(a);
-
+let a=100
+console.log (a);
+}
+let a=10,b=9
+console.log(a+b);
+let add= function (a,b) {
+    return a+b;
 };
+console.log (add (5,6))
+//გამოაცხადეთ ორი ცვლადი , მიანიჭეთ მნიშვნელობები 87 და 65, დაბეჭდეთ (console.log()) ამ რიცხვების ჯამი
+//გამოაცხადეთ ობიექტი student, ფროფერთებით: firstName, lastName, age, email, marks
+let add=function (a,b){
+    return a+b
+}; 
+console.log (add (87,65));
+
+let add=function (a,b,c){
+    return a+b-c
+};
+console.log (add (100,50,100));
+let a=23, b=4 
+console.log (a+b);
+let a=20,b=80
+console.log (a+b);
+let c= (a>b)
+console.log (c)
+let a=20,b=25,c=10
+console.log (a+b-c);
+let d= (a+b>c)
+console.log (d)
+let student = {
+    "Firstname" : "GIVI",
+    "Lastname" : "Kutibashvili",
+    "Email" :"Givdadada",
+    "Mark": "Blablabla",
+    "Adress": {country :"Georgia", City:"Tbilisi",},
+    "Age" : 31
+    }
+    console.log (student)/*
+
+    //გამოაცხადეთ ორი ცვლადი m და n მნიშვნელობებით 45 და 23, 
+    //დაბეჭდეთ მათი ჯამი თუ m მეტია n ზე , თუ არადა დაბეჭდეთ n.
+
+/*let m=45, n=23 
+console.log (m+n);
+console.log (m>n)*/
+
+
+//გამოაცხადეთ ცვლადი studentName და მიანიჭეთ მნიშვნელობად თქვენი სახელი. 
+//დაბეჭდეთ “Rise and shine, “თქვენი სახელი” !” 
+//( მაგ თუ სახელი არის Irakli  უნდა დაიბეჭდოს Rise and shine, Irakli ! ) 
